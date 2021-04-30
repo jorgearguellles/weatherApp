8e7e57ecb8fec6ef599478832f7142d7d79bda82
jorge arguelles <jorgeariasarguelles@gmail.com>
2021-04-29 22:25:31 -0500

    add/Place to visit Section & Top Review section DONE

--- src/components/secondSection/index.js
+++ src/components/secondSection/index.js
@@ -2,6 +2,9 @@ import React from "react";
 
 import styles from "./SecondSection.module.css";
 import CardForecastDay from "../CardForecastDay/index.js";
+import imgPin from "../../img/locationPin.png";
+import imgFace from "../../img/face.jpg";
+import imgMuseum from "../../img/museum.jpg";
 
 const SecondSection = () => {
 	return (
@@ -14,8 +17,48 @@ const SecondSection = () => {
 				<CardForecastDay day="Saturday" weather="Sun" weatherNumber="14/15" />
 				<CardForecastDay day="Sunday" weather="Rain" weatherNumber="30/25" />
 			</div>
-			<div className={styles.visitContainer}>Visit</div>
-			<div className={styles.reviewsContainer}>Reviews</div>
+			<div className={styles.visitContainer}>
+				<p className={styles.titleSection}>
+					<strong>Place to</strong> visit
+				</p>
+				<div className={styles.visitNamePlace}>
+					<img src={imgPin} alt="Pin location" />
+					<p>
+						Arab Street <br />
+						Singapore
+					</p>
+				</div>
+			</div>
+			<div className={styles.reviewsContainer}>
+				<div className={styles.titleContainer}>
+					<p className={styles.review}>+ Top Reviews</p>
+					<div className={styles.imgContainer}>
+						<img src={imgFace} alt="Foto de reviews" />
+						<img src={imgFace} alt="Foto de reviews" />
+						<img src={imgFace} alt="Foto de reviews" />
+						<p className={styles.plus}>6+</p>
+					</div>
+				</div>
+				<div className={styles.reviewPlace}>
+					<img src={imgPin} alt="Pin location" />
+					<p>
+						Art Science <br />
+						Museum
+					</p>
+				</div>
+				<div className={styles.reviewPlacePlus}>
+					<div className={styles.reviewPinContainer}>
+						<img src={imgPin} alt="Pin location" />
+						<p>
+							Art Science <br />
+							Museum
+						</p>
+					</div>
+					<div className={styles.reviewContainer}>
+						<p className={styles.reviewPlus}>+</p>
+					</div>
+				</div>
+			</div>
 			<div className={styles.otherPlacesContainer}>Paris</div>
 		</div>
 	);
