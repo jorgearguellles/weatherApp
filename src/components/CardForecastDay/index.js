d782a6aa1b555fc3c60aef2e0e55f024cb8f9852
jorge arguelles <jorgeariasarguelles@gmail.com>
2021-04-29 20:54:52 -0500

    add/cardForecastDay component

---
+++ src/components/CardForecastDay/index.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+import styles from "./CardForecastDay.module.css";
+import imgCloud from "../../img/cloud.png";
+import imgRain from "../../img/cloudRain.png";
+import imgSun from "../../img/sun.png";
+
+const CardForecastDay = (props) => {
+	return (
+		<div className={styles.dayContainer}>
+			<div className={styles.dayWeatherContainer}>
+				<img className={styles.imgWeather} src={imgSun} alt="img cloud" />
+				<div className={styles.letterContainer}>
+					<p className={styles.day}>{props.day}</p>
+					<p className={styles.weather}>{props.weather}</p>
+				</div>
+			</div>
+			<div className={styles.weatherNumberContainer}>
+				<p className={styles.weatherNumber}>{props.weatherNumber}</p>
+			</div>
+		</div>
+	);
+};
+
+export default CardForecastDay;
--- src/components/secondSection/index.js
+++ src/components/secondSection/index.js
@@ -1,7 +1,24 @@
 import React from "react";
 
+import styles from "./SecondSection.module.css";
+import CardForecastDay from "../CardForecastDay/index.js";
+
 const SecondSection = () => {
-	return <div>Second Section</div>;
+	return (
+		<div className={styles.container}>
+			<div className={styles.forecastContainer}>
+				<p className={styles.titleSection}>
+					<strong>3 Days</strong> Forecast
+				</p>
+				<CardForecastDay day="Friday" weather="Cloud" weatherNumber="34/45" />
+				<CardForecastDay day="Saturday" weather="Sun" weatherNumber="14/15" />
+				<CardForecastDay day="Sunday" weather="Rain" weatherNumber="30/25" />
+			</div>
+			<div className={styles.visitContainer}>Visit</div>
+			<div className={styles.reviewsContainer}>Reviews</div>
+			<div className={styles.otherPlacesContainer}>Paris</div>
+		</div>
+	);
 };
 
 export default SecondSection;
