4a4e8db6d67f3e3094f89cbc2a1952c4e5128785
jorge arguelles <jorgeariasarguelles@gmail.com>
2021-04-30 16:57:27 -0500

    add/mainSection done

--- src/components/mainSection/index.js
+++ src/components/mainSection/index.js
@@ -1,7 +1,27 @@
 import React from "react";
 
+import styles from "./MainSection.module.css";
+import imgPin from "../../img/locationPin.png";
+import imgSun from "../../img/sun.png";
+
 const MainSection = () => {
-	return <div>Main Section</div>;
+	return (
+		<div>
+			<div className={styles.weatherPlace}>
+				<img src={imgPin} alt="Pin location" />
+				<p>Bogotá</p>
+			</div>
+			<div className={styles.weatherPlaceContainer}>
+				<div className={styles.weatherImg}>
+					<img className={styles.imgWeather} src={imgSun} alt="img cloud" />
+				</div>
+				<div className={styles.centigrades}>
+					<p className={styles.weatherNumber}>31</p>
+					<p>ºC</p>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default MainSection;
