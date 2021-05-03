40009da50814b2247e514416c8e13c5a3ab0c416
jorge arguelles <jorgeariasarguelles@gmail.com>
2021-05-02 21:12:12 -0500

    update/ current day weather view all done with crazy external corners

--- src/components/mainSection/index.js
+++ src/components/mainSection/index.js
@@ -11,14 +11,20 @@ const MainSection = () => {
 				<img src={imgPin} alt="Pin location" />
 				<p>Bogotá</p>
 			</div>
-			<div className={styles.weatherPlaceContainer}>
-				<div className={styles.weatherImg}>
-					<img className={styles.imgWeather} src={imgSun} alt="img cloud" />
+			<div className={styles.bigContainer}>
+				<div className={styles.darkPurple}></div>
+				<div className={styles.darkPurpleTransparent}></div>
+				<div className={styles.weatherPlaceContainer}>
+					<div className={styles.weatherImg}>
+						<img className={styles.imgWeather} src={imgSun} alt="img cloud" />
+					</div>
+					<div className={styles.centigrades}>
+						<p className={styles.weatherNumber}>00</p>
+						<p>ºC</p>
+					</div>
 				</div>
-				<div className={styles.centigrades}>
-					<p className={styles.weatherNumber}>00</p>
-					<p>ºC</p>
-				</div>
+				<div className={styles.lightPurple}></div>
+				<div className={styles.lightPurpleTransparent}></div>
 			</div>
 		</div>
 	);
